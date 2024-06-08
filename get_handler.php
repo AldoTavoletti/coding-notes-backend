c91a724619969f24a28c569e09da5a8b81859d40
Aldo Tavoletti <aldotavoletti@gmail.com>
2024-06-08 10:23:55 +0200

    Update get_handler.php

--- get_handler.php
+++ get_handler.php
@@ -188,7 +188,7 @@ if (isset($_GET["retrieve"]) && $_GET["retrieve"] === "all") {
     if (check_logged_in($conn)) {
         echo json_encode(array("message" => "The user is logged in!", "username" => $_SESSION["username"], "code" => 200));
     }else{
-        echo json_encode(array("message" => "The user is not logged in!", "code" => 403));
+        echo json_encode(array("message" => "The user is not logged in!", "code" => 401));
 
     }
 
