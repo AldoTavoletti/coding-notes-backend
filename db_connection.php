da6d40d00097292d7a61ba7d851a6713a04d1f3a
Aldo Tavoletti <aldotavoletti@gmail.com>
2024-07-03 12:30:14 +0200

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,15 +1,32 @@
 <?php
 
-$servername = $_ENV["DB_HOSTNAME"];
-$userdb = $_ENV["DB_USERNAME"];
-$passworddb = $_ENV["DB_PASSWORD"];
-$dbname = $_ENV["DB_NAME"];
+// $servername = $_ENV["DB_HOSTNAME"];
+// $userdb = $_ENV["DB_USERNAME"];
+// $passworddb = $_ENV["DB_PASSWORD"];
+// $dbname = $_ENV["DB_NAME"];
 
 
-// Create connection 
-$conn = mysqli_connect($servername, $userdb, $passworddb, $dbname, 27516);
+// // Create connection 
+// $conn = mysqli_connect($servername, $userdb, $passworddb, $dbname, 27516);
 
-// Check connection 
+// // Check connection 
+// if (!$conn) {
+//     die("Connection failed: " . mysqli_connect_error());
+// }
+
+$fields = parse_url($_ENV["uri"]);
+
+// Extracting components from the URI
+$host = $fields["host"];
+$port = $fields["port"];
+$user = urldecode($fields["user"]);
+$pass = urldecode($fields["pass"]);
+$dbname = trim($fields["path"], '/'); // extracting dbname from path
+
+// Establishing mysqli connection
+$conn = mysqli_connect($host, $user, $pass, $dbname, $port);
+
+// Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
@@ -19,8 +36,9 @@ mysqli_ssl_set(
     $conn,
     NULL,
     NULL,
-    __DIR__ . "/ca.pem", // path to your ca.pem file
+    __DIR__ . "ca.pem", // path to your ca.pem file
     NULL,
     NULL
 );
+
 
