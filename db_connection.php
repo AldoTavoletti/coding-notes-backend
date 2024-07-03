1bc847cdf28f23a9abe38a29af390cf8bb521540
Aldo Tavoletti <aldotavoletti@gmail.com>
2024-07-03 12:02:39 +0200

    .

--- db_connection.php
+++ db_connection.php
@@ -5,6 +5,7 @@ $userdb = $_ENV["DB_USERNAME"];
 $passworddb = $_ENV["DB_PASSWORD"];
 $dbname = $_ENV["DB_NAME"];
 
+
 // Create connection 
 $conn = mysqli_connect($servername, $userdb, $passworddb, $dbname, 27516);
 
@@ -13,4 +14,13 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
+// Setting SSL options
+mysqli_ssl_set(
+    $conn,
+    NULL,
+    NULL,
+    "/ca.pem", // path to your ca.pem file
+    NULL,
+    NULL
+);
 
