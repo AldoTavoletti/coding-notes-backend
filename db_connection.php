34d937ed91fa03be45ff1a8fdcfeca8a9bf0eb88
Aldo Tavoletti <aldotavoletti@gmail.com>
2024-07-04 12:24:55 +0200

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,19 +1,5 @@
 <?php
 
-// $servername = $_ENV["DB_HOSTNAME"];
-// $userdb = $_ENV["DB_USERNAME"];
-// $passworddb = $_ENV["DB_PASSWORD"];
-// $dbname = $_ENV["DB_NAME"];
-
-
-// // Create connection 
-// $conn = mysqli_connect($servername, $userdb, $passworddb, $dbname, 27516);
-
-// // Check connection 
-// if (!$conn) {
-//     die("Connection failed: " . mysqli_connect_error());
-// }
-
 $fields = parse_url($_ENV["uri"]);
 
 // Extracting components from the URI
