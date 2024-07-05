8fc2f1b3b32d0847d11688a8ef3df946d93caa41
Aldo Tavoletti <aldotavoletti@gmail.com>
2024-07-05 18:36:01 +0200

    Update get_handler.php

--- get_handler.php
+++ get_handler.php
@@ -16,7 +16,6 @@ function get_all_folders_and_notes(mysqli $conn): ?array
     // fetch the whole result into an associative array
     $folders = $result->fetch_all(MYSQLI_ASSOC);
 
-    // get all the notes of the user's folders. The statement is prepared only once.
     $stmt = $conn->prepare("SELECT * FROM notes WHERE folderID=? ORDER BY noteIndex");
 
     // $folderID doesn't have a value now, but it gets assigned a value in the for loop. When the statement gets executed, the compiler looks for the binded parameters and uses the current value it is assigned to.
