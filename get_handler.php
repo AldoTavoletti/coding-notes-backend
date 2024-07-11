4a47da02ea4c6daac928c1b784830c89d954b8b9
Aldo Tavoletti <aldotavoletti@gmail.com>
2024-07-11 17:36:48 +0200

    Update get_handler.php

--- get_handler.php
+++ get_handler.php
@@ -170,7 +170,7 @@ function get_possible_notes(mysqli $conn, string $inputContent)
     if ($inputContent === "")
         return [];
     
-    $stmt = $conn->prepare("SELECT * FROM notes n INNER JOIN folders f ON n.folderID=f.folderID WHERE f.userID=? AND n.title LIKE ?");
+    $stmt = $conn->prepare("SELECT n.noteID, n.title, f.folderName, f.folderID FROM notes n INNER JOIN folders f ON n.folderID=f.folderID WHERE f.userID=? AND n.title LIKE ?");
     $inputContent = '%' . $inputContent . '%';
     $stmt->bind_param('is', $_SESSION['userID'], $inputContent);
     $stmt->execute();
