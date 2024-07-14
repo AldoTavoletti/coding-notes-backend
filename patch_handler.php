3e9ab64827315369d5b8ba65302552031f9bfa44
Aldo Tavoletti <aldotavoletti@gmail.com>
2024-07-14 11:22:12 +0200

    Update patch_handler.php

--- patch_handler.php
+++ patch_handler.php
@@ -20,8 +20,6 @@ function patch_note_title(mysqli $conn, string $title, int $noteID): void
 
     $stmt->execute();
 
-    $time = (string) time();
-    $_SESSION[$time.$title] = $title;
 }
 
 function patch_folder(mysqli $conn, string $name, string $color, int $folderID): void
@@ -98,8 +96,7 @@ if (isset($arr["content"])) /* if the content of the note has to be patched */ {
 
     patch_note_title($conn, $arr["title"], $arr["noteID"]);
 
-    // echo json_encode(array("message" => "Title updated!", "code" => 200, "title" => $arr["title"]));
-    echo json_encode($_SESSION);
+    echo json_encode(array("message" => "Title updated!", "code" => 200, "title" => $arr["title"]));
 
 
 } else if (isset($arr["name"], $arr["color"], $arr["folderID"])) /* if a folder has to be patched */ {
